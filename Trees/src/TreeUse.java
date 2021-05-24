69804f29cda718b59c0768654228fa35e93c9dff
Siddhant <siddhantbhalla28.sb@gmail.com>
2021-05-24 11:20:52 +0530

    trees

--- Trees/src/TreeUse.java
+++ Trees/src/TreeUse.java
@@ -40,5 +40,21 @@ public class TreeUse {
 		}
 		return count;
 	}
+	
+	public static int sumOfAllNode(TreeNode<Integer> root){
+		
+        if(root == null){
+            return 0;
+        }
+        
+        int sum = root.data;
+        
+        for(int i=0; i<root.children.size(); i++){
+            sum = sum + sumOfAllNode(root.children.get(i));
+        }
+        
+        return sum;
+        
+	}
 
 }
