a6b8153839dcdef82b638725cc4f8c7e29b4b456
Siddhant <siddhantbhalla28.sb@gmail.com>
2021-05-24 13:21:22 +0530

    trees

--- Trees/src/TreeUse.java
+++ Trees/src/TreeUse.java
@@ -27,6 +27,16 @@ public class TreeUse {
 		int numOfNodes = numNodes(root);
 		System.out.println(numOfNodes);
 		
+		int sum = sumOfAllNode(root);
+		System.out.println(sum);
+		
+		int largestNode = largest(root);
+		System.out.println(largestNode);
+		
+		preOrder(root);
+		System.out.println();
+		postOrder(root);
+		
 	}
 	
 	public static int numNodes(TreeNode<Integer> root) {
@@ -56,5 +66,101 @@ public class TreeUse {
         return sum;
         
 	}
+	
+	public static int largest(TreeNode<Integer> root) {
+		
+		if(root == null) {
+			return Integer.MIN_VALUE;  //return negative infinity
+		}
+		
+		int largestNode = root.data;
+		
+		for(int i=0; i<root.children.size(); i++) {
+			int largestChild = largest(root.children.get(i));
+			
+			if(largestChild>largestNode) {
+				largestNode = largestChild;
+			}
+		}
+		return largestNode;
+	}
+	
+	public static int getHeight(TreeNode<Integer> root) {
+		if(root==null) {
+			return 0;
+		}
+		int maxHeight = 0;
+		
+		for(int i=0; i<root.children.size(); i++) {
+			int max = getHeight(root.children.get(i));
+			
+			if(max>maxHeight) {
+				maxHeight = max;
+			}
+		}
+		
+		return maxHeight;
+	}
+	
+	public static void printNodeAtDepthK(TreeNode<Integer> root, int k) {
+		if(root==null) {
+			return;
+		}
+		if(k==0) {
+			System.out.println(root.data);
+			return;
+		}
+		for(int i=0; i<root.children.size(); i++) {
+			printNodeAtDepthK(root.children.get(i), k-1);
+		}
+		
+	}
+	
+	public static int countLeafNodes(TreeNode<Integer> root){
+
+		
+        if(root == null){
+            return 0;
+        }
+        
+        if(root.children.size() == 0){
+            return 1;
+        }
+        
+        int count = 0;
+        for(int i=0; i<root.children.size(); i++){
+            count += countLeafNodes(root.children.get(i));
+        }
+            
+        return count;
+
+	}
+	
+	public static void preOrder(TreeNode<Integer> root) {
+		
+		if(root==null) {
+			return;
+		}
+		
+		System.out.print(root.data + " ");
+		
+		for(int i=0; i<root.children.size(); i++) {
+			preOrder(root.children.get(i));
+		}
+
+	}
+	
+	public static void postOrder(TreeNode<Integer> root) {
+		
+		if(root == null) {
+			return;
+		}
+		
+		for(int i=0; i<root.children.size(); i++) {
+			postOrder(root.children.get(i));
+		}
+		System.out.print(root.data + " ");
+	}
+	
 
 }
