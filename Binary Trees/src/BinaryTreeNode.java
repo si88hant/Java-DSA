0f1e03985dd20493841213e213d07bc52f62c549
Siddhant <siddhantbhalla28.sb@gmail.com>
2021-05-25 05:43:35 +0530

    binary trees

--- Binary Trees/src/BinaryTreeNode.java
+++ Binary Trees/src/BinaryTreeNode.java
@@ -1,4 +1,12 @@
 
-public class BinaryTreeNode {
-
+public class BinaryTreeNode<T> {
+	
+	public T data;
+	public BinaryTreeNode<T> left;
+	public BinaryTreeNode<T> right;
+	
+	public BinaryTreeNode(T data) {
+		this.data = data;
+	}
+	
 }
