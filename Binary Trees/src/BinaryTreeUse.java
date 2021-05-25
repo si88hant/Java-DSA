1170b69703d152fd84f165020164e50190867701
Siddhant <siddhantbhalla28.sb@gmail.com>
2021-05-25 06:56:56 +0530

    binary trees

--- Binary Trees/src/BinaryTreeUse.java
+++ Binary Trees/src/BinaryTreeUse.java
@@ -33,4 +33,19 @@ public class BinaryTreeUse {
 		return totalNodes;
 		
 	}
+	
+	public static int height(BinaryTreeNode<Integer> root) {
+		if(root==null) {
+			return 0;
+		}
+		
+		int h1 = height(root.left);
+		int h2 = height(root.right);
+		
+		if(h1>h2) {
+			return 1+h1;
+		}else {
+			return 1+h2;
+		}
+	}
 }
