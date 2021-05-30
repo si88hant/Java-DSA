28e3d51298fab5e2afafb01206386f284fc5dffb
Siddhant <siddhantbhalla28.sb@gmail.com>
2021-05-30 07:07:23 +0530

    calculate Nth Fibonacci number

---
+++ Recursion/src/Fibonacci.java
@@ -0,0 +1,17 @@
+
+public class Fibonacci {
+
+	// print nth Fibonacci number
+
+	public int fib(int n) {
+		
+		if(n==1) {
+			return 0;
+		}
+		if(n==2) {
+			return 1;
+		}
+		
+		return fib(n-1) + fib(n-2);
+	}
+}
