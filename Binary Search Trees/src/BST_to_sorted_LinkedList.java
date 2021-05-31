65db15461645408651fc2f07b29bd9fe08e6ee93
Siddhant <siddhantbhalla28.sb@gmail.com>
2021-05-31 08:28:35 +0530

    construct a BST from a sorted array (correct solution)

--- Binary Search Trees/src/BST_to_sorted_LinkedList.java
+++ Binary Search Trees/src/BST_to_sorted_LinkedList.java
@@ -14,7 +14,7 @@ public class BST_to_sorted_LinkedList {
 
 		/*
 		 
-		 My Solution : Throwing null pointer exception 
+		 My Solution : Throwing null pointer exception --> because you didn't consider other cases
 		 
         if(root == null){
             return null;
@@ -38,6 +38,50 @@ public class BST_to_sorted_LinkedList {
         
         */
         
+		if(root == null) {
+			return null;
+		}
+		
+		LinkedListNode<Integer> midLL = new LinkedListNode<>(root.data);
+		
+		if(root.left == null && root.right == null) {
+			return midLL;
+		}
+		
+		else if(root.left != null && root.right !=null) {
+			LinkedListNode<Integer> left_head = constructLinkedList(root.left);
+			LinkedListNode<Integer> temp = left_head;
+			
+			while(temp.next != null) {
+				temp = temp.next;
+			}
+			temp.next = midLL;
+			
+			LinkedListNode<Integer> right_head = constructLinkedList(root.right);
+			midLL.next = right_head;
+			
+			return left_head;
+		}
+		
+		else if(root.left != null) {
+			LinkedListNode<Integer> left_head = constructLinkedList(root.left);
+			LinkedListNode<Integer> temp = left_head;
+			
+			while(temp.next != null) {
+				temp = temp.next;
+			}
+			temp.next = midLL;
+			
+			return left_head;
+		}
+		else {
+			LinkedListNode<Integer> right_head = constructLinkedList(root.right);
+			midLL.next = right_head;
+			
+			return midLL; 
+
+		}
+		
 	}	
 	
 }
