d7acd9cc709f73473c68488e546d75500f67c4c5
Siddhant <siddhantbhalla28.sb@gmail.com>
2021-06-08 07:02:42 +0530

    Find kth smallest element in an array

--- Arrays/src/kth_smallest_element.java
+++ Arrays/src/kth_smallest_element.java
@@ -7,9 +7,9 @@ public class kth_smallest_element {
 		int arr[] = {36,29,82,552,89,12,36,783,15,79};
 		Arrays.sort(arr);
 		
-		int k = 2;
+		int k = 4;
 		
-		System.out.println("kth smallest element is: " + arr[k-1]);
+		System.out.println(k+"th smallest element is: " + arr[k-1]);
 	}
 
 }
