a84f1e0d79bde9ee2412758047a8c8e252433d50
Siddhant <siddhantbhalla28.sb@gmail.com>
2021-06-18 04:46:27 +0530

    detect loop in Linked List (2nd solution)

--- Linked List/src/Easy/Detect_loop_in_LL.java
+++ Linked List/src/Easy/Detect_loop_in_LL.java
@@ -27,4 +27,26 @@ public class Detect_loop_in_LL {
         }
         return false;
     }
+    
+    
+    //solution 2
+    
+    public static boolean detectLoop2(Node head){
+        if(head == null){
+            return false;
+        }
+        
+        Node slow = head;
+        Node fast = head;
+        
+        while(slow.next!=null && fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }    
 }
