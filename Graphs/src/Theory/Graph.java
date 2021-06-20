539966943a694b66fa8c1b73ff8b3974209f5dc2
Siddhant <siddhantbhalla28.sb@gmail.com>
2021-06-20 12:41:04 +0530

    graphs theory

---
+++ Graphs/src/Theory/Graph.java
@@ -0,0 +1,51 @@
+package Theory;
+
+//by riya bansal unacademy
+
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class Graph {
+	
+	private class Vertex{
+		//Neighbors
+		HashMap<String, Integer> ngbrs = new HashMap<>();
+	}
+	
+	private HashMap<String, Vertex> vertices = new HashMap<>();
+	
+	public int numVertex() {
+		return vertices.size();
+	}
+	
+	public void addVertex(String vname) {
+		Vertex vtx = new Vertex();
+		vertices.put(vname, vtx);
+	}
+	
+	public void addEdge(String vname1, String vname2, int cost) {
+		Vertex vtx1 = vertices.get(vname1);
+		Vertex vtx2 = vertices.get(vname2);
+
+		
+		//if one of the nodes are null or if nodes are already connected
+		if(vtx1 == null || vtx2 == null || vtx1.ngbrs.containsKey(vname2)) {
+			return;
+		}
+		
+		vtx1.ngbrs.put(vname2,  cost);
+		vtx2.ngbrs.put(vname1,  cost);
+	}
+	
+	public void display() {
+		System.out.println("-----------");
+		ArrayList<String> keys = new ArrayList<>(vertices.keySet());
+		
+		for(String key:keys) {
+			Vertex vtx = vertices.get(key);
+			
+			String str = key + " -> " + vtx.ngbrs;
+			System.out.println(str);
+		}
+	}
+}
---
+++ Graphs/src/Theory/GraphClient.java
@@ -0,0 +1,31 @@
+package Theory;
+
+public class GraphClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Graph graph = new Graph();
+		
+		graph.addVertex("A");
+		graph.addVertex("B");
+		graph.addVertex("C");
+		graph.addVertex("D");
+		graph.addVertex("E");
+		graph.addVertex("F");
+		graph.addVertex("G");
+		
+		graph.addEdge("A", "B", 1);
+		graph.addEdge("A", "D", 10);
+		graph.addEdge("B", "C", 2);
+		graph.addEdge("C", "D", 3);
+		graph.addEdge("D", "E", 4);
+		graph.addEdge("E", "F", 5);
+		graph.addEdge("E", "G", 12);
+		graph.addEdge("F", "G", 4);
+		
+		graph.display();
+	}
+
+}
