8a3bedcfbd4557aa3deb7bcbfa5882a9febcd762
Siddhant <siddhantbhalla28.sb@gmail.com>
2021-06-22 11:37:38 +0530

    Queue using 2 stacks

---
+++ Stacks & Queues/src/Easy/Queue_using_2_stacks.java
@@ -0,0 +1,40 @@
+package Easy;
+
+import java.util.*;
+
+public class Queue_using_2_stacks {
+
+    Stack<Integer> s1 = new Stack<Integer>();
+    Stack<Integer> s2 = new Stack<Integer>();
+
+    //Function to push an element in queue by using 2 stacks.
+    void Push(int x)
+    {
+	   s1.push(x);
+    }
+	
+    
+    //Function to pop an element from queue by using 2 stacks.
+    int Pop()
+    {   
+	   if(s1.empty() && s2.empty()){
+	       return -1;     //return -1 if the queue is empty
+	   }
+	   
+	   if(s2.empty()){
+         while(!s1.empty())
+            {	   
+                int temp = s1.pop();
+	            s2.push(temp);
+            }
+            return s2.pop();
+	   }
+	   
+	   
+	   //else{
+            return s2.pop();  //if s2 is not empty then pop from s2
+	   //}
+        
+    }	
+	
+}
