e94d306dcba231318236f4675d16ffd980edc079
Siddhant <siddhantbhalla28.sb@gmail.com>
2021-06-27 07:44:05 +0530

    DFS

--- Graphs/src/Easy/DFS.java
+++ Graphs/src/Easy/DFS.java
@@ -1,5 +1,34 @@
 package Easy;
 
-public class DFS {
+import java.util.ArrayList;
 
+public class DFS {
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj)
+    {
+        // Code here
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        boolean[] vis = new boolean[V];
+        trav(adj,a,0,vis);
+        return a;
+    }
+    
+    public void trav(ArrayList<ArrayList<Integer>> adj , ArrayList<Integer> a , int s , boolean[] vis){
+
+        if(vis[s] == true){
+            return;
+        }
+        
+        vis[s]=true;
+        a.add(s);
+        
+        for(Integer i : adj.get(s)){
+
+            if(vis[i] == false){
+
+                trav(adj , a , i,vis);
+
+            }
+
+        }
+    }
 }
