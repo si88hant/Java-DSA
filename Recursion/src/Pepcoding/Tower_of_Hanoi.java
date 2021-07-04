c25143e3d02e11a61bedc4d874d993b37f1c0a9a
Siddhant <siddhantbhalla28.sb@gmail.com>
2021-07-04 13:39:35 +0530

    Tower of Hanoi

---
+++ Recursion/src/Pepcoding/Tower_of_Hanoi.java
@@ -0,0 +1,27 @@
+package Pepcoding;
+
+import java.util.Scanner;
+
+public class Tower_of_Hanoi {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int t1d = sc.nextInt();
+        int t2d = sc.nextInt();
+        int t3d = sc.nextInt();
+        toh(n, t1d, t2d, t3d);
+        
+    }
+
+    public static void toh(int n, int t1id, int t2id, int t3id){
+        if(n==0){
+            return;
+        }
+        
+        toh(n-1, t1id, t3id, t2id);
+        System.out.println(n + "[" + t1id + " -> " + t2id + "]");
+        toh(n-1, t3id, t2id, t1id);
+    }
+}
