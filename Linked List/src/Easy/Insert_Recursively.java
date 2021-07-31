8434e2f88c75858f6b9acb0a7181571c52be6c49
Siddhant <siddhantbhalla28.sb@gmail.com>
2021-07-31 09:12:21 +0530

    Insert a node recursively in a LL

--- Linked List/src/Easy/Insert_Recursively.java
+++ Linked List/src/Easy/Insert_Recursively.java
@@ -1,10 +1,23 @@
 package Easy;
 
 public class Insert_Recursively {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static Node<Integer> insertRec(Node<Integer> head, int pos, int elem){
+ 		
+		if(head == null) {
+			return head;
+		}
+		
+		//base case
+		if(pos == 0) {
+			Node<Integer> newHead = new Node<>(elem);
+			newHead.next = head;
+			return newHead;
+		}
+		
+		head.next = insertRec(head.next, pos-1, elem);
+		
+		return head;
 	}
 
 }
