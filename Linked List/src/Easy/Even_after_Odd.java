2177c9e6c509cae57bbe8a93cfeb1bafcd929749
Siddhant <siddhantbhalla28.sb@gmail.com>
2021-08-02 10:46:32 +0530

    Even after odd LL

--- Linked List/src/Easy/Even_after_Odd.java
+++ Linked List/src/Easy/Even_after_Odd.java
@@ -2,56 +2,48 @@ package Easy;
 
 public class Even_after_Odd {
 	public static LinkedListNode<Integer> sortEvenOdd(LinkedListNode<Integer> head) {
-		   		LinkedListNode<Integer> odd = null, oddTail = null;
-				LinkedListNode<Integer> even = null, evenTail = null;
-				LinkedListNode<Integer> curr = head;
-
-			while (curr != null)
-				{
-					if ((curr.data & 1) == 0) // current node is even
-					{
-						// handle head for first odd node
-						if (odd == null) {
-							odd = oddTail = curr;
-						}
-						else
-						{
-							oddTail.next = curr;
-							oddTail = oddTail.next;
-						}
-					}
-					else // current node is odd
-					{
-						// handle head for first even node
-						if (even == null) {
-							even = evenTail = curr;
-						}
-						else
-						{
-							evenTail.next = curr;
-							evenTail = curr;
-						}
-					}
-					curr = curr.next;
-				}
-
-				// if list contains at-least one even node
-				if (even != null)
-				{
-					head = even;
-					evenTail.next = odd;
-				}
-				// special case - list contains all odd nodes
-				else {
-					head = odd;
-				}
-
-				// null to terminate the list, 
-				// else it will go in infinite loop
-				if (oddTail != null) {
-					oddTail.next = null;
-				}
-
-				return head;
-			}
+       
+		LinkedListNode<Integer> evenHead = null, evenTail = null, oddHead = null, oddTail = null;
+        
+        while(head != null){
+            
+            if(head.data%2 ==0){ //if head's data is even
+                
+               if(evenHead == null){
+                   evenHead = head;
+                   evenTail = head;
+               }else{
+					evenTail.next = head;
+                    evenTail = evenTail.next;
+               }
+                
+            }
+            else{ //if head's data is odd
+                
+                if(oddHead == null){
+                    oddHead = head;
+                    oddTail = head;
+                }else{
+                    oddTail.next = head;
+                    oddTail = oddTail.next;
+                }
+            	
+            }
+            
+            head = head.next;
+        }
+            
+        if(oddHead == null){
+            return evenHead;
+        }
+        else{
+            oddTail.next = evenHead;
+        }
+        
+        if(evenHead != null){
+            evenTail.next = null;
+        }
+        
+        return oddHead; 
+	}
 }
