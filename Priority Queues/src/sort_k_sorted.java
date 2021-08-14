94e42b3f75b03e5ee64ceb2eefa78699ba233949
Siddhant <siddhantbhalla28.sb@gmail.com>
2021-08-14 10:43:52 +0530

    Sort k sorted array

---
+++ Priority Queues/src/sort_k_sorted.java
@@ -0,0 +1,36 @@
+import java.util.PriorityQueue;
+
+public class sort_k_sorted {
+
+	//Time complexity = O(nlogk)
+	
+	public static void main(String[] args) {
+		
+		int arr[] = {2,4,1,9,6,8};
+		int k = 3;
+		
+		sortKSorted(arr, k);
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+	public static void sortKSorted(int arr[] , int k) {
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		
+		int i=0;
+		
+		for( ; i < k; i++) {
+			pq.add(arr[i]);
+		}
+		
+		for( ; i < arr.length; i++) {
+			arr[i-k] = pq.remove();
+			pq.add(arr[i]);
+		}
+		
+		for(int j = arr.length-k; j < k; j++) {
+			arr[j] = pq.remove();
+		}
+	}
+}
