5769578ee1551dc9ecfb4a1c94177170931aeea0
Siddhant <siddhantbhalla28.sb@gmail.com>
2021-08-19 14:25:05 +0530

    Min steps to one DP 2nd method

--- Dynamic Programming/src/Min_Steps_To_One_DP.java
+++ Dynamic Programming/src/Min_Steps_To_One_DP.java
@@ -6,22 +6,41 @@ public class Min_Steps_To_One_DP {
         int storage[] = new int[n+1];
         storage[1] = 0;
         
-        for(int i=2; i<n+1; i++){
-            int min = storage[i-1];
-            
-            if(i%3 == 0){
-                if(storage[i/3] < min){
-                    min = storage[i/3];
-                }
-            }
-            if(i%2 == 0){
-                if(storage[i/2] < min){
-                    min = storage[i/2];
-                }
-            }            
-            storage[i] = 1 + min;
+//        for(int i=2; i<n+1; i++){
+//            int min = storage[i-1];
+//            
+//            if(i%3 == 0){
+//                if(storage[i/3] < min){
+//                    min = storage[i/3];
+//                }
+//            }
+//            if(i%2 == 0){
+//                if(storage[i/2] < min){
+//                    min = storage[i/2];
+//                }
+//            }            
+//            storage[i] = 1 + min;
+//        }
+//        return storage[n];
+        
+        //SECOND METHOD //prateek narang
+        
+        for(int i=2; i<=n; i++) {
+        	int op1 = Integer.MAX_VALUE;
+        	int op2 = Integer.MAX_VALUE;
+        	int op3 = Integer.MAX_VALUE;
+        	
+        	if(n%3 == 0) {
+        		op1 = storage[i/3];
+        	}
+        	if(n%2 == 0) {
+        		op2 = storage[i/2];
+        	}
+        	op3 = storage[i-1];
+        	
+        	storage[i] = Math.min(op1, Math.min(op2, op3)) + 1;
         }
         return storage[n];
-	}
 	
+	}
 }
