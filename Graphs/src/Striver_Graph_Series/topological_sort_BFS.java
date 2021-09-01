79c110e20e69ea276a1d253657e8ce5863dff585
Siddhant <siddhantbhalla28.sb@gmail.com>
2021-09-01 16:32:26 +0530

    Topological sort using BFS (Kahn's Algorithm)

---
+++ Graphs/src/Striver_Graph_Series/topological_sort_BFS.java
@@ -0,0 +1,41 @@
+package Striver_Graph_Series;
+
+import java.util.*;
+
+public class topological_sort_BFS {
+
+    public int[] topoSort(int N, ArrayList<ArrayList<Integer>> adj) {
+    	
+        int topo[] = new int[N]; 
+        int indegree[] = new int[N]; 
+        
+        for(int i = 0;i<N;i++) {
+            for(Integer it: adj.get(i)) {
+                indegree[it]++; 
+            }
+        }
+        
+        Queue<Integer> q = new LinkedList<Integer>(); 
+        for(int i = 0;i<N;i++) {
+            if(indegree[i] == 0) {
+                q.add(i); 
+            }
+        }
+        
+        int i = 0;
+        while(!q.isEmpty()) {
+            Integer node = q.poll(); 
+            topo[i] = node;
+            i++; 
+            
+            for(Integer it: adj.get(node)) {
+                indegree[it]--; 
+                if(indegree[it] == 0) {
+                    q.add(it); 
+                }
+            }
+        }
+        return topo; 
+    }
+	
+}
