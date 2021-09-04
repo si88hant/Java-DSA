ee1a600a6eeff49e780dfb7bfbc00225024cb8fb
Siddhant <siddhantbhalla28.sb@gmail.com>
2021-09-04 15:12:51 +0530

    Print all paths from source to destination

---
+++ Graphs/src/Pepcoding/Print_all_Paths.java
@@ -0,0 +1,67 @@
+package Pepcoding;
+
+import java.util.*;
+import java.io.*;
+
+public class Print_all_Paths {
+	
+	   static class Edge {
+		      int src;
+		      int nbr;
+		      int wt;
+
+		      Edge(int src, int nbr, int wt) {
+		         this.src = src;
+		         this.nbr = nbr;
+		         this.wt = wt;
+		      }
+		   }
+
+		   public static void main(String[] args) throws Exception {
+		      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		      int vtces = Integer.parseInt(br.readLine());
+		      ArrayList<Edge>[] graph = new ArrayList[vtces];
+		      for (int i = 0; i < vtces; i++) {
+		         graph[i] = new ArrayList<>();
+		      }
+
+		      int edges = Integer.parseInt(br.readLine());
+		      for (int i = 0; i < edges; i++) {
+		         String[] parts = br.readLine().split(" ");
+		         int v1 = Integer.parseInt(parts[0]);
+		         int v2 = Integer.parseInt(parts[1]);
+		         int wt = Integer.parseInt(parts[2]);
+		         graph[v1].add(new Edge(v1, v2, wt));
+		         graph[v2].add(new Edge(v2, v1, wt));
+		      }
+
+		      int src = Integer.parseInt(br.readLine());
+		      int dest = Integer.parseInt(br.readLine());
+
+		      // write all your codes here
+		      
+		      boolean vis[] = new boolean[vtces];
+			  printAllPaths(graph, src, dest, vis, src + "");
+		   }
+	
+	
+	
+    public static void printAllPaths(ArrayList<Edge>[] graph, int src, int dest, boolean[] vis, String psf) {
+		
+		if(src == dest) {
+			System.out.println(psf);
+			return;
+		}
+		
+		vis[src] = true;
+		
+		for(Edge edge : graph[src]) {
+			if(vis[edge.nbr] == false) {
+				printAllPaths(graph, edge.nbr, dest, vis, psf + edge.nbr);
+			}
+		}
+		vis[src] = false;
+	}
+	
+}
