5586f1f43204d6f142b9a14c48854eb025fe9339
Siddhant <siddhantbhalla28.sb@gmail.com>
2021-09-22 14:24:28 +0530

    Subsequences of a string

---
+++ Recursion/src/Aditya_Verma_recursion/print_subsequences_of_a_string.java
@@ -0,0 +1,34 @@
+package Aditya_Verma_recursion;
+
+import java.util.Scanner;
+
+public class print_subsequences_of_a_string {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String input = sc.nextLine();
+		String output = "";
+		
+		solve(input, output);
+	}
+
+	public static void solve(String ip, String op) {
+		
+		if(ip.length() == 0) {
+			System.out.print(op + " ");
+			return;
+		}
+		
+		String op1 = op;
+		String op2 = op;
+		
+		op1 += ip.charAt(0);
+		ip = ip.substring(1);
+		
+		solve(ip, op1);
+		solve(ip, op2);
+		
+	}
+	
+}
